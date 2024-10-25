405958b1c2b393e13c3839bef71e948a247180c1
malek604 <soheilmehrad1379@gmail.com>
2024-10-25 21:34:52 +0330

    Add project files.

---
+++ MauiApp1/Crawling.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net;
+
+
+
+
+
+namespace MauiApp1
+{
+    internal class Crawling
+    {
+        private readonly HttpClient _httpClient;
+        public string Body;
+        public string Headers2;
+        public string Error;
+        public string ClientHeader;
+        public Crawling ()
+        {
+            _httpClient = new HttpClient();
+
+        } 
+        public void  DownloadContentAsync(string url) 
+        {
+            try
+            {
+                var response = _httpClient.GetAsync(url).Result;
+                var headers = new StringBuilder();
+
+                var body = response.Content.ReadAsStringAsync();
+                Body = response.Content.ReadAsStringAsync().Result;
+                var requestheader = new StringBuilder();
+
+                foreach (var header in _httpClient.DefaultRequestHeaders)
+                {
+                    requestheader.AppendLine($"{header.Key}: {string.Join(", ", header.Value)}");
+                }
+                foreach (var header in response.Headers)
+                {
+                    headers.AppendLine($"{header.Key}: {string.Join(", ", header.Value)}");
+                }   
+                Headers2 = headers.ToString();
+                ClientHeader = requestheader.ToString();
+
+
+            }
+            catch (Exception ex) {
+                Error = ex.Message;
+
+            }
+        }
+
+        public List<string> GetURLs( string url) 
+        {
+            var urls = new List<string>();
+            try
+            {
+                DownloadContentAsync(url);
+
+                var document = new HtmlDocument();
+                document.LoadHtml(Body);
+
+                var baseuri = new Uri(url);
+                var host = baseuri.Host;
+
+                var links = document.DocumentNode.SelectNodes("//a[@href]");
+
+                if (links != null)
+                {
+                    foreach (var link in links)
+                    {
+                        var hrefvalue = link.GetAttributeValue("href", string.Empty);
+                        var absoluteuri = new Uri(baseuri, hrefvalue);
+                        if (absoluteuri.Host == host)
+                        {
+                            urls.Add(absoluteuri.ToString());
+                        }
+                    }
+
+
+                }
+            }
+            catch (Exception ex) { Error = ex.Message; }
+            return urls;
+
+        }
+        public List<string> GetImageURLs(string url)
+        {
+            var ImgUrls = new List<string>();
+            try
+            {
+                DownloadContentAsync(url);
+                var document = new HtmlDocument();
+
+                document.LoadHtml(Body);
+                var baseuri = new Uri(url);
+                var imagenodes = document.DocumentNode.SelectNodes("//img[@src]");
+                if(imagenodes != null)
+                {
+                    foreach (var imagen in imagenodes)
+                    {
+                        var srcvalue = imagen.GetAttributeValue("src", string.Empty);
+                        var absoluteUri = new Uri(baseuri, srcvalue);
+                        ImgUrls.Add(absoluteUri.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Error = ex.Message;
+            }
+            return ImgUrls;
+        }
+        public string ProcessDataAsync(string url, DataType dataType)
+        {
+            return "null";
+        }
+    }
+
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,225 @@
+﻿
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Microsoft.Maui.Storage;
+namespace MauiApp1
+{
+    public enum DataType
+    {
+        Images,
+        Links,
+        //Text
+    }
+
+
+    public partial class MainPage : ContentPage
+    {
+        List<string> links = new List<string>();
+        private Crawling _webCrawler;
+        private string _headers;
+        private string _url;
+        private string _body;
+        string whatSelected;
+        string urlPattern = @"^(https?:\/\/)?([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}(\/\S*)?$";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _webCrawler = new Crawling();
+        }
+
+        // Event handler برای تغییر انتخاب Picker
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            var selectedIndex = picker.SelectedIndex;
+
+            // بررسی اینکه آیا ایندکس انتخاب شده معتبر است
+            if (selectedIndex != -1)
+            {
+                whatSelected = picker.SelectedItem.ToString();
+                PickerSelectedWhatData.Text = whatSelected;
+            }
+        }
+
+        // سایر متدها
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+
+            urlEntry.Text = string.Empty;
+            resultEditor.Text = string.Empty;
+
+        }
+
+        private async void OnCopyClicked(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(resultEditor.Text))
+            {
+                await Clipboard.SetTextAsync(resultEditor.Text);
+                await DisplayAlert("Copy", "Content copied to clipboard", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Copy", "No content to copy", "OK");
+            }
+        }
+
+        private async void OnScrapeClicked(object sender, EventArgs e)
+        {
+            crawlButton.IsEnabled = false;
+            crawlButton.IsVisible = false;
+            loadingIndicator.IsRunning = true;
+            loadingIndicator.IsVisible = true;
+            string url = urlEntry.Text;
+            if (!string.IsNullOrEmpty(url))
+            {
+
+
+                if (Regex.IsMatch(url, urlPattern))
+                {
+                    if (!string.IsNullOrEmpty(whatSelected))
+                    {
+                        if (Enum.TryParse<DataType>(whatSelected, out var SelectedDataType))
+                        {
+
+                            switch (SelectedDataType)
+                            {
+                                case DataType.Links:
+                                    await Task.Run(() => links = _webCrawler.GetURLs(url));
+                                    foreach (var item in links)
+                                    {
+                                        resultEditor.Text += $"\n{item}";
+                                    }
+
+                                    break;
+                                    /*
+                                case DataType.Text:
+                                    await Task.Run(() => _webCrawler.DownloadContentAsync(url));
+                                    break;
+                                    */
+                                case DataType.Images:
+                                    await Task.Run(() => links = _webCrawler.GetImageURLs(url));
+                                    ShowImage(links);
+                                    foreach (var item in links)
+                                    {
+                                        resultEditor.Text += $"\n{item}";
+                                    }
+                                    break;
+                            }
+
+                        }
+
+
+
+                        _headers = _webCrawler.Headers2;
+                        _body = _webCrawler.Body;
+
+
+                    }
+                    else
+                    {
+                        await DisplayAlert("What search??", "No Selected item", "OK");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("What search??", "Invalid Url", "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("What search??", "Url is Empty", "OK");
+            }
+            loadingIndicator.IsVisible = false;
+            loadingIndicator.IsRunning = false;
+            crawlButton.IsVisible = true;
+            crawlButton.IsEnabled = true;
+
+
+        }
+        private void OnHeadersClicked(object sender, EventArgs e)
+        {
+
+            resultEditor.Text = $"Server Header : \n {_headers} \n ClientHeader :{_webCrawler.ClientHeader}";
+        }
+
+        private void OnBodyClicked(object sender, EventArgs e)
+        {
+            resultEditor.Text = _body;
+        }
+        private void OnDataClicked(object sender, EventArgs e)
+        {
+            foreach (var item in links)
+            {
+                resultEditor.Text += $"\n{item}";
+            }
+        }
+        private void ShowImage(List<string> imageUrls)
+        {
+            imageContainer.Children.Clear();
+
+            foreach (var url in imageUrls)
+            {
+                // ایجاد یک VerticalStackLayout برای هر تصویر و دکمه آن
+                var stackLayout = new VerticalStackLayout
+                {
+                    Margin = new Thickness(0, 10)
+                };
+
+                // ایجاد تصویر
+                var image = new Image
+                {
+                    Source = ImageSource.FromUri(new Uri(url)),
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    Aspect = Aspect.AspectFit,
+                    Margin = new Thickness(0, 1)
+                };
+
+                // ایجاد دکمه دانلود
+                var downloadButton = new Button
+                {
+                    Text = "Download Image",
+                    HorizontalOptions = LayoutOptions.Center,
+                    Margin = new Thickness(0, 5)
+                };
+
+                // رویداد کلیک دکمه دانلود
+                downloadButton.Clicked += (sender, e) =>
+                {
+                    DownloadImage(url);
+                };
+
+                // اضافه کردن تصویر و دکمه به stackLayout
+                /*
+                stackLayout.Children.Add(image);
+                stackLayout.Children.Add(downloadButton);
+                */
+                // اضافه کردن stackLayout به imageContainer
+                imageContainer.Children.Add(image);
+            }
+        }
+
+        private async void DownloadImage(string Imageurl)
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                var ImageBytes = await httpClient.GetByteArrayAsync(Imageurl);
+                var filename = Path.Combine(FileSystem.CacheDirectory, Path.GetFileName(Imageurl));
+                File.WriteAllBytes(filename, ImageBytes);
+
+                await DisplayAlert("Download Complete", $"Image saved to {filename}", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Failed to download image: {ex.Message}", "OK");
+            }
+
+        }
+    }
+}
